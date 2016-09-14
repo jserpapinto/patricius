7e76998947f88f0679881fb81ceddb395118fc15
amzuh <daniel.ferreira026835@gmail.com>
2016-09-14 02:36:23 +0100

    aa

---
+++ client/routers.js
@@ -0,0 +1,29 @@
+var App = angular.module('App', ['ngRoute']);
+// configure routes
+App.config(function($routeProvider, $locationProvider) {
+	$routeProvider
+		// route for the home page
+		.when('/', {
+			templateUrl : 'pages/home.html',
+			controller  : 'mainController'
+		})
+		.when('/tournmant', {
+			templateUrl : 'pages/tournmant.html',
+			controller  : 'tournmantController'
+		})
+		.otherwise({redirectTo: 'pages/home.html'})
+
+	// html5 history api
+	$locationProvider.html5Mode(true);
+});
+
+
+App.controller('mainController', function($scope) {
+	console.log("in main home");
+});
+
+App.controller('tournmantController', function($scope) {
+	console.log("tournmantController");
+});
+
+
